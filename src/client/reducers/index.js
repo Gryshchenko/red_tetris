8fb78f03a4e7c109aa5905df9d3e76b6aa0dfc16
gryshchenko <gryshchenko.89@gmail.com>
2019-04-15 19:12:45 +0300

    add data too room redux

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,13 +1,18 @@
 import { fromJS } from 'immutable';
 
+const ACTION_TYPE = {
+    CREATE_NEW_PLAYER: 'server/createNewPlayer',
+};
+
 const intialState = {
     room: null,
     games: null,
 }
 
 const reducer = (state = fromJS(intialState), action) => {
+    console.warn(action);
     switch (action.type) {
-        case 'CREATE_NEW_PLAYER':
+        case ACTION_TYPE.CREATE_NEW_PLAYER:
             return state.setIn(['room'], fromJS(action.data));
         case 'CREATE_NEW_ROOM':
             return;
