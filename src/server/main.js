5c0d61be4687310f13eb99df63a7eb8a20ef3a48
ekiriche <evgeniy.kirichenko@ticktech.bg>
2019-04-24 17:46:06 +0300

    srv main file fix

--- src/server/main.js
+++ src/server/main.js
@@ -7,11 +7,11 @@ import event from './socket/index.js';
 
 require("babel-core/register");
 require("babel-polyfill");
+
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const app = express();
 
 const server = require('http').createServer(app);
 
