82eb05531adb2790db61c3f6bfa9c78adfa7a68a
gryshchenko <gryshchenko.89@gmail.com>
2019-05-05 15:08:05 +0300

    tetris view

--- src/client/components/Room/Room.js
+++ src/client/components/Room/Room.js
@@ -7,20 +7,44 @@ import { GameBordInfo } from '../gameBordInfo/gameBordInfo';
 const Room = () => {
   return (
     <div className={'roomMain'}>
-      <div className={'roomMainFlex'}>
-        <div>
-          <GameBord />
+      <div className={'tetrisViewMain'}>
+        <div className={'roomMainFlex'}>
+          <div>
+            <GameBord />
+          </div>
+          <div>
+            <GameBordInfo />
+          </div>
         </div>
-        <div>
-          <GameBordInfo />
+        <div className={'control'}>
+          <div className={'generalButton'}>
+            <div className={'topButton'}>
+              <div className={'tetrisButton tetrisButtonSmall'} />
+              <div className={'tetrisButton tetrisButtonSmall'} />
+              <div className={'tetrisButton tetrisButtonSmall'} />
+            </div>
+            <div className={'spaceButton'}>
+              <div className={'tetrisButton tetrisButtonBig'} />
+            </div>
+          </div>
+
+          <div className={'moveButton'}>
+            <div className={'tetrisButton tetrisButtonMiddle'} />
+            <div className={'moveButtonMiddle'} >
+              <div className={'tetrisButton tetrisButtonMiddle flex1'} />
+              <div className={'flex1'} />
+              <div className={'tetrisButton tetrisButtonMiddle flex1'} />
+            </div>
+            <div className={'tetrisButton tetrisButtonMiddle'} />
+          </div>
         </div>
       </div>
-      {/* <div>
-        <EnemyBord />
-      </div> */}
     </div>
   );
 }
 export default (Room);
 
+{/* <div>
+<EnemyBord />
+</div> */}
 
